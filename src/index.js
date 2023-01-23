abc3aba2b8f34cca044b98c1a89109410d1027c0
Mur <murmavilink@gmail.com>
2023-01-23 13:40:42 +0600

    made search.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import getData from "./modules/getData";
 import { modal } from "./modules/modal";
 import { sortCards } from "./modules/sortCards";
+import { search } from "./modules/search";
 
 getData();
 modal();
 sortCards();
+search();
