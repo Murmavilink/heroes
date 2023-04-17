fa8b12a971bd38be9ffef2ee6b4dec1885bad337
Mur <murmavilink@gmail.com>
2023-04-18 00:16:05 +0600

    run build

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,7 @@
 export const getData = async () => {
     
     try {
-        let resp = await fetch('https://marvelheroesdb-default-rtdb.firebaseio.com/heroes.json');
+        let resp = await fetch('https://marvelheroes-3c25f-default-rtdb.firebaseio.com/heroes.json');
         return await resp.json();
     } catch (error) {
         console.log(error);
